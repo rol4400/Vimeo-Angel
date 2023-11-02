dc3eebc7b6b2bb444c64c33345c888cd9d64bbaf
Ryan Olsen <rol4400@outlook.com>
2023-11-02 22:59:03 +1000

    Fixed settings updating

--- src/index.ts
+++ src/index.ts
@@ -47,39 +47,66 @@ bot.on('video', (ctx) => {
 });
 
 // Handle button clicks
-bot.action(/edit_(\w+)/, (ctx) => {
-  const userId = getUserId(ctx);
-  if (!userId) {
-    console.error('Unable to determine user ID');
-    return;
-  }
-
-  const setting = ctx.match[1];
-
-  switch (setting) {
-    case 'date':
-      ctx.reply('📅 Please enter the date in the format YYMMDD:');
-      break;
-
-    case 'password':
-      ctx.reply('🔐 Please enter the password:');
-      break;
-
-    case 'title':
-      ctx.reply('📝 Please enter the title:');
-      break;
-
-    case 'leader':
-      ctx.reply('👤 Please enter the leader:');
-      break;
-
-    case 'destination':
-      ctx.reply('🌍 Please select a destination:', Markup.inlineKeyboard(
-        destinations.map(dest => [Markup.button.callback(`📍 ${dest}`, `select_destination_${dest}`)])
-      ));
-      break;
-  }
-});
+bot.on('callback_query', (ctx) => {
+    const userId = getUserId(ctx);
+    if (!userId) {
+      console.error('Unable to determine user ID');
+      return;
+    }
+  
+    // Use type assertion to tell TypeScript that data exists
+    const action = (ctx.callbackQuery as any).data;
+  
+    switch (action) {
+      case 'edit_date':
+        ctx.reply('📅 Please enter the date in the format YYMMDD:',
+        {
+            reply_markup: {
+                force_reply: true,
+                input_field_placeholder: "YYMMDD",
+            },
+        },);
+        break;
+  
+      case 'edit_password':
+        ctx.reply('🔐 Please enter the password:',
+        {
+            reply_markup: {
+                force_reply: true,
+                input_field_placeholder: "Password",
+            },
+        },);
+        break;
+  
+      case 'edit_title':
+        ctx.reply('📝 Please enter the title:',
+        {
+            reply_markup: {
+                force_reply: true,
+                input_field_placeholder: "Education Title",
+            },
+        },);
+        break;
+  
+      case 'edit_leader':
+        ctx.reply('👤 Please enter the leader:',
+        {
+            reply_markup: {
+                force_reply: true,
+                input_field_placeholder: "Education Leader Name & Title",
+            },
+        },);
+        break;
+  
+      case 'edit_destination':
+        ctx.reply('🌍 Please select a destination:', Markup.inlineKeyboard(
+          destinations.map(dest => [Markup.button.callback(`📍 ${dest}`, `select_destination_${dest}`)])
+        ));
+        break;
+    }
+  });
+  
+  
 
 // Handle destination selection
 bot.action(/select_destination_(.+)/, (ctx) => {
@@ -102,19 +129,16 @@ bot.action(/select_destination_(.+)/, (ctx) => {
 bot.on('text', (ctx) => {
     const userId = getUserId(ctx);
     if (!userId) {
-      console.error('Unable to determine user ID');
-      return;
+        console.error('Unable to determine user ID');
+        return;
     }
-  
+
     const chatId = ctx.chat.id;
     const text = ctx.message.text;
-  
+
     // Handle specific setting inputs
     handleSettingInput(ctx, userId, text);
-  
-    // Note: Show settings panel should be placed here to ensure the settings are updated before displaying the panel
-  });
-    
+});
 
 // Function to show the settings panel
 function showSettingsPanel(ctx, chatId) {
@@ -152,84 +176,99 @@ function showSettingsPanel(ctx, chatId) {
 
 // Function to handle setting inputs
 function handleSettingInput(ctx, userId, input) {
-    const lowercaseInput = input.toLowerCase();
-  
-    switch (lowercaseInput) {
-      case 'complete':
-        // Save settings and perform necessary actions
-        // For now, just log the settings
-        console.log(`Settings for user ${userId}:`, userSettings[userId]);
-  
-        // Reset user settings
-        userSettings[userId] = {};
-  
-        // Show settings panel
-        showSettingsPanel(ctx, ctx.chat.id);
-        break;
-  
-      case 'cancel':
-        // Reset user settings
-        userSettings[userId] = {};
-  
-        // Show settings panel
-        showSettingsPanel(ctx, ctx.chat.id);
-        break;
-  
-      default:
-        // Handle specific setting inputs
-        updateSetting(ctx, userId, lowercaseInput);
-        break;
-    }
-  }
+    const lowercaseInput = ctx.update.message.text;
 
+    // Use a simplified regex to extract the setting
+    const match = ctx.update.message.reply_to_message.text.match(/.*Please enter the (\w+)/);
+    console.log(match);
+
+    switch (lowercaseInput) {
+        case 'complete':
+            // Save settings and perform necessary actions
+            // For now, just log the settings
+            console.log(`Settings for user ${userId}:`, userSettings[userId]);
+
+            // Reset user settings
+            userSettings[userId] = {};
+
+            break;
+
+        case 'cancel':
+            // Reset user settings
+            userSettings[userId] = {};
+
+            break;
+
+        default:
+            // Handle specific setting inputs
+            updateSetting(ctx, userId, lowercaseInput, match);
+            break;
+    }
+}
 
 // Function to update specific settings
-function updateSetting(ctx, userId, input) {
+function updateSetting(ctx, userId, input, match) {
     const userSetting = userSettings[userId];
-  
+
     // Determine which setting to update based on the context
-    if (ctx.match && ctx.match[1]) {
-      const setting = ctx.match[1];
-  
-      switch (setting) {
-        case 'edit_date':
-          // Validate and update the date setting
-          if (/^\d{6}$/.test(input)) {
-            userSetting.date = input;
-            ctx.reply('Date updated successfully.');
-          } else {
-            ctx.reply('Invalid date format. Please enter the date in the format YYMMDD:');
-          }
-          break;
-  
-        case 'edit_password':
-          // Update the password setting
-          userSetting.password = input;
-          ctx.reply('Password updated successfully.');
-          break;
-  
-        case 'edit_title':
-          // Update the title setting
-          userSetting.title = input;
-          ctx.reply('Title updated successfully.');
-          break;
-  
-        case 'edit_leader':
-          // Update the leader setting
-          userSetting.leader = input;
-          ctx.reply('Leader updated successfully.');
-          break;
-  
-        default:
-          // Handle other settings if needed
-          break;
-      }
+    if (match && match[1]) {
+        const setting = match[1];
+
+        switch (setting) {
+            case 'date':
+                // Validate and update the date setting
+                const year = Number(input.substring(0, 2));
+                const month = Number(input.substring(2, 4));
+                const day = Number(input.substring(4, 6));
+
+                if (
+                    /^\d{6}$/.test(input) &&
+                    year >= 40 &&
+                    month >= 1 && month <= 12 && // Validating month
+                    day >= 1 && day <= new Date(year + 2000, month, 0).getDate() // Validating day based on the month
+                ) {
+                    userSetting.date = input;
+                    ctx.reply('Date updated successfully.');
+                    showSettingsPanel(ctx, ctx.message.chat.id);
+                } else {
+                    ctx.reply('Invalid date format. You must use the format YYMMDD.');
+                    ctx.reply('📅 Please enter the date:',
+                    {
+                        reply_markup: {
+                            force_reply: true,
+                            input_field_placeholder: "YYMMDD",
+                        },
+                    },);
+                }
+                break;
+
+            case 'password':
+                // Update the password setting
+                userSetting.password = input;
+                ctx.reply('Password updated successfully.');
+                showSettingsPanel(ctx, ctx.message.chat.id);
+                break;
+
+            case 'title':
+                // Update the title setting
+                userSetting.title = input;
+                ctx.reply('Title updated successfully.');
+                showSettingsPanel(ctx, ctx.message.chat.id);
+                break;
+
+            case 'leader':
+                // Update the leader setting
+                userSetting.leader = input;
+                ctx.reply('Leader updated successfully.');
+                showSettingsPanel(ctx, ctx.message.chat.id);
+                break;
+
+            default:
+                // Handle other settings if needed
+                break;
+        }
     }
-  
-    // Show settings panel
-    showSettingsPanel(ctx, ctx.chat!.id);
-  }
-  
+}
 
 // Function to get user ID from context
 function getUserId(ctx) {
