0d2a27e911f21f74d373b0500897fe65b754f659
Ryan Olsen <rol4400@outlook.com>
2023-11-04 17:19:26 +1000

    Started setting up file monitor client

---
+++ src/folder_monitor/renderer.js
@@ -0,0 +1,10 @@
+// renderer.js
+const { ipcRenderer } = window;
+
+// Example: Send a message to the main process
+ipcRenderer.send('renderer-message', 'Hello from the renderer process!');
+
+// Example: Receive a message from the main process
+ipcRenderer.on('main-message', (event, data) => {
+  console.log('Received message in renderer process:', data);
+});
